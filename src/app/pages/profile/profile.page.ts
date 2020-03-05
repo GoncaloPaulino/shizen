9f57d4233dd406e91932b8ee578db9b2a17ffae5
Gonçalo Paulino <grpaulino8@gmail.com>
2020-03-05 15:15:05 +0000

    Profile page v0.5

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-profile',
@@ -10,12 +11,34 @@ export class ProfilePage {
 
   user = null;
  
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService,
+    private alertCtrl: AlertController) {
     this.user = this.auth.getUser();
   }
  
   logout() {
     this.auth.logout();
+  }
+
+  changename(){
+    this.showAlert("Manutenção", "Função temporariamente indisponível!");
+  }
+
+  changemail(){
+    this.showAlert("Manutenção", "Função temporariamente indisponível!");
+  }
+
+  changepass(){
+    this.showAlert("Manutenção", "Função temporariamente indisponível!");
+  }
+
+  async showAlert(hdr: string, msg: string){
+    let alert = await this.alertCtrl.create({
+      header: hdr,
+      message: msg,
+      buttons: ['Ok']
+    });
+    alert.present();
   }
 
 }
